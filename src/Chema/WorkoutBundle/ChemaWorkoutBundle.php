799ffb56f8bf0a584b35a2543c81dcad5288757f
chema navrus <chemanuell6@gmail.com>
2014-08-24 11:49:07 -0430

    2do commit
    creacion bundle Chema

---
+++ src/Chema/WorkoutBundle/ChemaWorkoutBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Chema\WorkoutBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ChemaWorkoutBundle extends Bundle
+{
+}
---
+++ src/Chema/WorkoutBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Chema\WorkoutBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
