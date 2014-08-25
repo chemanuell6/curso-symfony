321e0abcaa20fee267ef533d2095aaf530443b4c
chema navrus <chemanuell6@gmail.com>
2014-08-24 20:27:22 -0430

    4to commit (rutas). cap8
    rutas con o sin parámetros, prefijos...

--- src/Chema/WorkoutBundle/Controller/DefaultController.php
+++ src/Chema/WorkoutBundle/Controller/DefaultController.php
@@ -5,17 +5,24 @@ namespace Chema\WorkoutBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+*@  @Route("/default")
+*/
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/")
-     * @Template()
+     * @Route("/hello/{name}/{lastName}", name="ruta_1")	
+     * Method("Get","Post")
+     * @Route("/hello/", name="ruta_2")
      */
-    public function indexAction()
+    public function indexAction($lastName="no tiene", $name="algo")
     {
-        return new Response ('hola mundo!');
+ 	   $response = new Response(json_encode(array('name' => $name,'lastName' => $lastName)));
+	   $response->headers->set('Content-Type', 'application/json');
+	   return $response;
     }
 }
