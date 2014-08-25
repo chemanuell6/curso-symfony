dce75030a6807c19d8c25aa712a6503357f2e2cb
chema navrus <chemanuell6@gmail.com>
2014-08-25 09:57:20 -0430

    7mo commit. doctrine parte 2
    inserción de datos en la base de datos

--- src/Chema/WorkoutBundle/Controller/DefaultController.php
+++ src/Chema/WorkoutBundle/Controller/DefaultController.php
@@ -5,48 +5,28 @@ namespace Chema\WorkoutBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Response;
-
+use Chema\WorkoutBundle\Entity\Workout;
 
 /**
-* @Route("workout")
-*/
-
+ * @Route("workout")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="workout_index")	
-     * Template("")
+     * @Route("/", name="workout_index")
+     * @Template("")
      */
     public function indexAction()
     {
- 	   $workouts= array(
-        array(
-                'date' => new \DateTime(),
-                'activity'=> 'swimming',
-                'hours' => 1
-            ),
-        array(
-                'date' => new \DateTime(),
-                'activity' => 'yoga',
-                'hours' => 2
-            ),
-        array(
-                'date' => new \DateTime(),
-                'activity' => 'gim',
-                'hours' => 1.5
-            ),
-        array(
-                'date' => new \DateTime(),
-                'activity' => 'running',
-                'hours' => 0.5 
-            ),
-        );
-    
-    return array (
-            'workouts' => $workouts,
-            'name' => 'Your Name',
-            'age' => 99
-        );
+        $workout = new Workout();
+        $workout->setActivity('yoga');
+        $workout->setHours(1);
+        $workout->setOccurrenceDate(new \DateTime());
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($workout);
+        $em->flush();        
+        
+        return array();
     }
 }
