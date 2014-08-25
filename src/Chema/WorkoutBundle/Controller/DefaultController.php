75b19109a451470284634c3908f216ca38061cf4
chema navrus <chemanuell6@gmail.com>
2014-08-24 22:07:33 -0430

    5to commit. Vistas twig (problema con el return)
    manejo de twig

--- src/Chema/WorkoutBundle/Controller/DefaultController.php
+++ src/Chema/WorkoutBundle/Controller/DefaultController.php
@@ -5,24 +5,48 @@ namespace Chema\WorkoutBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 
+
 /**
-*@  @Route("/default")
+* @Route("workout")
 */
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}/{lastName}", name="ruta_1")	
-     * Method("Get","Post")
-     * @Route("/hello/", name="ruta_2")
+     * @Route("/", name="workout_index")	
+     * Template("")
      */
-    public function indexAction($lastName="no tiene", $name="algo")
+    public function indexAction()
     {
- 	   $response = new Response(json_encode(array('name' => $name,'lastName' => $lastName)));
-	   $response->headers->set('Content-Type', 'application/json');
-	   return $response;
+ 	   $workouts= array(
+        array(
+                'date' => new \DateTime(),
+                'activity'=> 'swimming',
+                'hours' => 1
+            ),
+        array(
+                'date' => new \DateTime(),
+                'activity' => 'yoga',
+                'hours' => 2
+            ),
+        array(
+                'date' => new \DateTime(),
+                'activity' => 'gim',
+                'hours' => 1.5
+            ),
+        array(
+                'date' => new \DateTime(),
+                'activity' => 'running',
+                'hours' => 0.5 
+            ),
+        );
+    
+    return array (
+            'workouts' => $workouts,
+            'name' => 'Your Name',
+            'age' => 99
+        );
     }
 }
